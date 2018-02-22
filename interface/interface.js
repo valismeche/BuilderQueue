b0f882438596874921c264b3b5458491d4094682
mafrazz <mafrazzrafael@gmail.com>
2018-02-21 22:29:51 -0300

    Added functional settings interface with local storage.

--- interface/interface.js
+++ interface/interface.js
@@ -3,12 +3,16 @@ define('TWOverflow/Builder/interface', [
     'TWOverflow/locale',
     'TWOverflow/Interface',
     'TWOverflow/FrontButton',
+    'TWOverflow/eventQueue',
+    'TWOverflow/utils',
     'ejs'
 ], function (
     Builder,
     Locale,
     Interface,
     FrontButton,
+    eventQueue,
+    utils,
     ejs
 ) {
     // Controlador de interface
@@ -16,26 +20,222 @@ define('TWOverflow/Builder/interface', [
     // Controlador do botão para abrir a janela da interface
     var opener
 
-    function BuilderInterface () {
-        var buildingOrder = {
-            headquarter: 0,
-            timber_camp: 1,
-            clay_pit: 2,
-            iron_mine: 3,
-            farm: 4,
-            warehouse: 5,
-            chapel: 6,
-            church: 7,
-            rally_point: 8,
-            barracks: 9,
-            statue: 10,
-            hospital: 11,
-            wall: 12,
-            market: 13,
-            tavern: 14,
-            academy: 15,
-            preceptory: 16
+    var groups
+
+    var $window
+    var $sequence
+    var $sequenceBody
+    var $groupVillages
+    var $settings
+    var $save
+
+    var disabled
+
+    var buildingOrder = {
+        headquarter: 0,
+        timber_camp: 1,
+        clay_pit: 2,
+        iron_mine: 3,
+        farm: 4,
+        warehouse: 5,
+        chapel: 6,
+        church: 7,
+        rally_point: 8,
+        barracks: 9,
+        statue: 10,
+        hospital: 11,
+        wall: 12,
+        market: 13,
+        tavern: 14,
+        academy: 15,
+        preceptory: 16
+    }
+
+    var updateGroupVillages = function () {
+        var $selectedOption = $groupVillages.find('.custom-select-handler').html('')
+        var $data = $groupVillages.find('.custom-select-data').html('')
+
+        appendDisabledOption($data, '0')
+
+        for (var id in groups) {
+            var name = groups[id].name
+            var selected = Builder.settings.groupVillages == id
+
+            if (Builder.settings.groupVillages === '') {
+                $selectedOption.html(disabled)
+                $groupVillages[0].dataset.name = disabled
+                $groupVillages[0].dataset.value = ''
+            } else if (Builder.settings.groupVillages == id) {
+                $selectedOption.html(name)
+                $groupVillages[0].dataset.name = name
+                $groupVillages[0].dataset.value = id
+            }
+
+            appendSelectData($data, {
+                name: name,
+                value: id,
+                icon: groups[id].icon
+            })
+
+            $groupVillages.append($data)
         }
+
+        if (!Builder.settings.groupVillages) {
+            $selectedOption.html(disabled)
+        }
+
+    }
+
+    /**
+     * Gera uma opção "desativada" padrão em um custom-select
+     *
+     * @param  {jqLite} $data - Elemento que armazenada o <span> com dataset.
+     * @param {String=} _disabledValue - Valor da opção "desativada".
+     */
+    var appendDisabledOption = function ($data, _disabledValue) {
+        var dataElem = document.createElement('span')
+        dataElem.dataset.name = disabled
+        dataElem.dataset.value = _disabledValue || ''
+
+        $data.append(dataElem)
+    }
+
+    /**
+     * Popula o dataset um elemento <span>
+     *
+     * @param  {jqLite} $data - Elemento que armazenada o <span> com dataset.
+     * @param  {[type]} data - Dados a serem adicionados no dataset.
+     */
+    var appendSelectData = function ($data, data) {
+        var dataElem = document.createElement('span')
+
+        for (var key in data) {
+            dataElem.dataset[key] = data[key]
+        }
+
+        $data.append(dataElem)
+    }
+
+    /**
+     * Loop em todas configurações do BuilderQueue
+     *
+     * @param {Function} callback
+     */
+    var eachSetting = function (callback) {
+        $window.find('[data-setting]').forEach(function ($input) {
+            var settingId = $input.dataset.setting
+
+            callback($input, settingId)
+        })
+
+        callback($sequence, 'sequence')
+    }
+
+    var saveSettings = function () {
+        var newSettings = {}
+
+        eachSetting(function ($input, settingId) {
+            var inputType = Builder.settingsMap[settingId].inputType
+
+            switch (inputType) {
+            case 'text':
+                newSettings[settingId] = $input.type === 'number'
+                    ? parseInt($input.value, 10)
+                    : $input.value
+
+                break
+            case 'select':
+                newSettings[settingId] = $input.dataset.value
+
+                break
+            case 'checkbox':
+                newSettings[settingId] = $input.checked
+
+                break
+            case 'sequence':
+                var newSequence = []
+                var $levels = $input.find('td.level')
+
+                $levels.forEach(function ($level) {
+                    var building = $level.dataset.building
+                    var level = parseInt($level.innerHTML, 10)
+
+                    newSequence.push([building, level])
+                })
+
+                newSettings[settingId] = newSequence
+
+                break
+            }
+        })
+
+        if (Builder.updateSettings(newSettings)) {
+            utils.emitNotif('success', Locale('builder', 'settings.saved'))
+
+            return true
+        }
+
+        return false
+    }
+
+    /**
+     * Insere as configurações na interface.
+     */
+    var populateSettings = function () {
+        eachSetting(function ($input, settingId) {
+            var inputType = Builder.settingsMap[settingId].inputType
+
+            switch (inputType) {
+            case 'text':
+                $input.value = Builder.settings[settingId]
+
+                break
+            case 'select':
+                $input.dataset.value = Builder.settings[settingId]
+
+                break
+            case 'checkbox':
+                if (Builder.settings[settingId]) {
+                    $input.checked = true
+                    $input.parentElement.classList.add('icon-26x26-checkbox-checked')
+                }
+
+                break
+            case 'sequence':
+                appendSequence()
+
+                break
+            }
+        })
+    }
+
+    var appendSequence = function () {
+        Builder.settings.sequence.forEach(function (item) {
+            var building = item[0]
+            var buildingLevel = item[1]
+            var buildingIndex = buildingOrder[building]
+            var $row = document.createElement('tr')
+            var $cell
+
+            for (var cellIndex = 0; cellIndex < 17; cellIndex++) {
+                $cell = document.createElement('td')
+
+                if (buildingIndex === cellIndex) {
+                    $cell.className = 'level'
+                    $cell.dataset.building = building
+                    $cell.innerHTML = buildingLevel
+                }
+
+                $row.appendChild($cell)
+            }
+
+            $sequenceBody.append($row)
+        })
+    }
+
+    function BuilderInterface () {
+        groups = modelDataService.getGroupList().getGroups()
+        disabled = Locale('builder', 'general.disabled')
 
         ui = new Interface('BuilderQueue', {
             activeTab: 'info',
@@ -57,27 +257,22 @@ define('TWOverflow/Builder/interface', [
             ui.openWindow()
         })
 
-        var $window = $(ui.$window)
-        var $sequence = $window.find('.sequence')
-        var $sequenceBody = $sequence.find('tbody')
+        $window = $(ui.$window)
+        $sequence = $window.find('.sequence')
+        $sequenceBody = $sequence.find('tbody')
+        $groupVillages = $window.find('.groupVillages')
+        $settings = $window.find('.settings')
+        $save = $window.find('.save')
 
-        Builder.buildingSequence.forEach(function (item) {
-            var $row = document.createElement('tr')
-            var $cell
-            var buildingIndex = buildingOrder[item[0]]
+        populateSettings()
+        updateGroupVillages()
 
-            for (var cellIndex = 0; cellIndex < 17; cellIndex++) {
-                $cell = document.createElement('td')
+        $save.on('click', function (event) {
+            saveSettings()
+        })
 
-                if (buildingIndex === cellIndex) {
-                    $cell.className = 'level'
-                    $cell.innerHTML = item[1]
-                }
-
-                $row.appendChild($cell)
-            }
-
-            $sequenceBody.append($row)
+        eventQueue.bind('Builder/groupsChanged', function () {
+            updateGroupVillages()
         })
 
         return ui
--- source/builder.js
+++ source/builder.js
@@ -10,18 +10,18 @@ define('TWOverflow/Builder', [
     Lockr
 ) {
     /**
+     * Indica se o BuilderQueue já foi inicializado.
+     *
+     * @type {Boolean}
+     */
+    var initialized = false
+
+    /**
      * Métodos e propriedades publicas do BuilderQueue.
      *
      * @type {Object}
      */
     var Builder = {}
-
-    /**
-     * Indica se o BuilderQueue já foi inicializado.
-     *
-     * @type {Boolean}
-     */
-    Builder.initialized = false
 
     /**
      * Versão atual do BuilderQueue
@@ -31,29 +31,95 @@ define('TWOverflow/Builder', [
     Builder.version = '__builder_version'
 
     /**
+     * Atualiza as configurações com os dados passados pelo usuário.
+     *
+     * @param {Object} changes - Novas configurações.
+     */
+    Builder.updateSettings = function (changes) {
+        // TODO
+        // analisar a sequencia para checar conflitos de leveis.
+
+        for (var key in changes) {
+            var newValue = changes[key]
+
+            if (newValue === Builder.settings[key]) {
+                continue
+            }
+
+            Builder.settings[key] = newValue
+        }
+
+        Lockr.set('builder-settings', Builder.settings)
+
+        return true
+    }
+
+    /**
      * Inicializa o BuilderQueue.
      */
     Builder.init = function () {
         Locale.create('builder', __builder_locale, 'en')
 
-        Builder.initialized = true
+        initialized = true
 
-        console.log('Builder initialized!')
+        /**
+         * Configurações salvas localmente
+         *
+         * @type {Object}
+         */
+        var localSettings = Lockr.get('builder-settings', {}, true)
 
-        Builder.buildingSequence = [
-            ['timber_camp', 2],
-            ['clay_pit', 2],
-            ['iron_mine', 2],
-            ['farm', 2],
-            ['warehouse', 2],
-            ['timber_camp', 3],
-            ['clay_pit', 3],
-            ['iron_mine', 3],
-            ['warehouse', 3],
-            ['timber_camp', 4],
-            ['clay_pit', 4],
-            ['iron_mine', 4]
-        ]
+        Builder.settingsMap = {
+            groupVillages: {
+                default: '',
+                inputType: 'select'
+            },
+            sequence: {
+                default: [
+                    ['timber_camp', 2],
+                    ['clay_pit', 2],
+                    ['iron_mine', 2],
+                    ['farm', 2],
+                    ['warehouse', 2],
+                    ['timber_camp', 3],
+                    ['clay_pit', 3],
+                    ['iron_mine', 3],
+                    ['warehouse', 3],
+                    ['timber_camp', 4],
+                    ['clay_pit', 4],
+                    ['iron_mine', 4]
+                ],
+                inputType: 'sequence'
+            }
+        }
+
+        /**
+         * Configurações do jogador + configurações padrões
+         *
+         * @type {Object}
+         */
+        Builder.settings = {}
+
+        for (var key in Builder.settingsMap) {
+            var defaultValue = Builder.settingsMap[key].default
+
+            Builder.settings[key] = localSettings.hasOwnProperty(key)
+                ? localSettings[key]
+                : defaultValue
+        }
+
+        rootScope.$on(eventTypeProvider.GROUPS_UPDATED, function () {
+            eventQueue.trigger('Builder/groupsChanged')
+        })
+    }
+
+    /**
+     * Retorna se o BuilderQueue já foi inicializado.
+     *
+     * @return {Boolean}
+     */
+    Builder.isInitialized = function () {
+        return initialized
     }
 
     return Builder
